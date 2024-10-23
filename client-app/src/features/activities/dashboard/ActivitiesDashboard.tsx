d5c8ed4c907b0533302551a254f82f36d54e9c34
mastertope <tupengabs@gmail.com>
2024-10-24 07:09:59 +0800

    End of Section 6

--- client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
@@ -14,11 +14,12 @@ interface Props {
     closeForm: () => void,
     createOrEdit: (activity: Activity) => void,
     deleteActivity: (id: string) => void,
+    submitting: boolean,
 }
 
 export default function ActivitiesDashboard({activities, selectedActivity, selectActivity, 
-    cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, 
-    deleteActivity}: Readonly<Props>) {
+    cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, 
+    submitting}: Readonly<Props>) {
     return (
         <Grid>
             <Grid.Column width={"10"}>
@@ -26,6 +27,7 @@ export default function ActivitiesDashboard({activities, selectedActivity, selec
                     activities={activities} 
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width={"6"}>
@@ -40,6 +42,7 @@ export default function ActivitiesDashboard({activities, selectedActivity, selec
                         closeForm={closeForm}
                         activity={selectedActivity}
                         createOrEdit={createOrEdit}
+                        submitting={submitting}
                     />
                 }
             </Grid.Column>
