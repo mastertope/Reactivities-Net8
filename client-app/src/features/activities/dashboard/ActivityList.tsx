7f4fcf361de6675e29f4632aff797a85da00a197
mastertope <tupengabs@gmail.com>
2024-10-24 12:53:37 +0800

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -2,6 +2,7 @@ import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { SyntheticEvent, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
+import { Link } from "react-router-dom";
 
 export default observer(function ActivityList() {  
     const { activityStore } = useStore();
@@ -35,7 +36,7 @@ export default observer(function ActivityList() {
                                     content="Delete"
                                     color="red"
                                 />
-                                <Button onClick={() => activityStore.selectActivity(activity.id)}
+                                <Button as={Link} to={`/activities/${activity.id}`}
                                     floated="right"
                                     content="View"
                                     color="blue" />
